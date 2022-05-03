79aade2180eba5867a4a27d4a336b7314e966fe3
ItsToxicGGAlt <104733482+ItsToxicGGAlt@users.noreply.github.com>
2022-05-03 20:53:29 +0200

    Create PingCommand.php

---
+++ src/LDFx/ItsToxicGG/LDCommand/PingCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace LDFx\ItsToxicGG\LDCommand;
+
+use pocketmine\Server;
+use pocketmine\player\Player;
+use pocketmine\plugin\Plugin;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginOwned;
+
+use LDFx\ItsToxicGG\LDFx;
+
+class PingCommand extends Command implements PluginOwned{
+    
+    private $plugin;
+
+    public function __construct(LDFx $plugin){
+        $this->plugin = $plugin;
+        
+        parent::__construct("ping", "§r§fGet Your Ping With LDFx, By ItsToxicGG", "§cUse: /ping", ["ping"]);
+        $this->setAliases(["pg"]);
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args){
+        if(count($args) == 0){
+            if($sender instanceof Player) {
+					      if ($this->getServer()->getPlayer($args[0])) {
+						         $player = $this->getServer()->getPlayer($args[0]);
+						         $this->plugin->sendPing($sender, $player->getPing());
+						         return true;
+            } else {
+                     $sender->sendMessage("Use this command in-game");
+            }
+        }
+        return true;
+    }
+    
+    public function getPlugin(): Plugin{
+        return $this->plugin;
+    }
+
+    public function getOwningPlugin(): LDFx{
+        return $this->plugin;
+    }
+}
