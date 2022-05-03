de3340f8ccbc9a85c72d6ab0cc57274e98e04881
ItsToxicGGAlt <104733482+ItsToxicGGAlt@users.noreply.github.com>
2022-05-03 20:40:21 +0200

    Update HPingTask.php

--- src/LDFx/ItsToxicGG/LDTask/HPingTask.php
+++ src/LDFx/ItsToxicGG/LDTask/HPingTask.php
@@ -6,20 +6,5 @@ use LDFx\ItsToxicGG\LDFx;
 use pocketmine\scheduler\Task;
 
 class HPingTask extends Task {
-
-    private LDFx $plugin;
-
-    public function __construct(LDFx $LDFx) {
-        $this->plugin = $LDFx;
-    }
-
-    public function onRun() : void {
-        if ($this->plugin->getServer()->getTicksPerSecondAverage() >= $this->plugin->getConfig()->get("tps-check")) {
-            foreach ($this->plugin->getServer()->getOnlinePlayers() as $player) {
-                if ($player->getNetworkSession()->getPing() >= $this->plugin->getConfig()->get("ping-kick")) {
-                    $player->kick($this->plugin->getConfig()->get("kick-message"));
-                }
-            }
-        }
-    }
+  // s
 }
