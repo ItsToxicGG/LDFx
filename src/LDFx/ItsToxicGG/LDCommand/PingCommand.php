b17cb3e029307d6f86d6666b57716c318374d5ce
ItsToxicGGAlt <104733482+ItsToxicGGAlt@users.noreply.github.com>
2022-05-03 20:59:33 +0200

    ?

--- src/LDFx/ItsToxicGG/LDCommand/PingCommand.php
+++ src/LDFx/ItsToxicGG/LDCommand/PingCommand.php
@@ -25,16 +25,25 @@ class PingCommand extends Command implements PluginOwned{
 
     public function execute(CommandSender $sender, string $commandLabel, array $args){
         if(count($args) == 0){
-            if($sender instanceof Player) {
-					      if ($this->getServer()->getPlayer($args[0])) {
-						         $player = $this->getServer()->getPlayer($args[0]);
-						         $this->plugin->sendPing($sender, $player->getPing());
-						         return true;
-            } else {
-                     $sender->sendMessage("Use this command in-game");
-            }
-        }
-        return true;
+            if($sender instanceof Player) 
+		if ($this->getServer()->getPlayer($args[0])) {
+		     $player = $this->getServer()->getPlayer($args[0]);
+		     $this->plugin->sendPing($sender, $player->getPing());
+		     return true;
+		} else {
+		     $sender->sendMessage(TextFormat::RED . "Player not found");
+		}
+	} else {
+		if ($sender instanceof Player) {
+		     $this->sendPing($sender, $sender->getPing());
+		     return true;
+		} else {
+		     $sender->sendMessage(TextFormat::RED . "Please enter a player name");
+		}
+	}
+	break;
+      }
+      return false;
     }
     
     public function getPlugin(): Plugin{
