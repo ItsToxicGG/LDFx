a3588b07b5140b9975e013e3c020c53223e177e5
Toxic.Co <104733482+ItsToxicGGAlt@users.noreply.github.com>
2022-05-04 17:05:02 +0200

    Update EventListener.php

--- src/LDFx/ItsToxicGG/LDEvent/EventListener.php
+++ src/LDFx/ItsToxicGG/LDEvent/EventListener.php
@@ -10,7 +10,7 @@ use LDFx\ItsToxicGG\LDFx;
 use function str_replace;
 use function time;
 
-class EventListener extends PluginBase implements Listener {
+class EventListener implements Listener {
   
   public $plugin;	
 	
