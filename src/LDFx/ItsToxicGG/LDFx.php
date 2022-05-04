2517916941f789d07edaa4df81198fe4d448c454
Toxic.Co <104733482+ItsToxicGGAlt@users.noreply.github.com>
2022-05-04 20:50:49 +0200

    hmmm

--- src/LDFx/ItsToxicGG/LDFx.php
+++ src/LDFx/ItsToxicGG/LDFx.php
@@ -9,7 +9,6 @@ use LDFx\ItsToxicGG\LDCommand\NickColorCommand;
 use LDFx\ItsToxicGG\LDCommand\GUICommand;
 use LDFx\ItsToxicGG\LDCommand\SocialMenuCommand;
 use LDFx\ItsToxicGG\LDTask\HPingTask;
-use LDFx\ItsToxicGG\LDTask\HAlwaysDayTask;
 use LDFx\ItsToxicGG\LDEvent\EventListener;
 use LDFx\ItsToxicGG\LDUtils\PluginUtils;
 // POCKETMINE
@@ -70,8 +69,7 @@ class LDFx extends PluginBase implements Listener
  
   public function onEnable(): void{
       $this->getLogger()->info("§aEnabled LDFx");
-      $this->getServer()->getPluginManager()->registerEvents($this, $this);
-      $this->getServer()->getPluginManager()->registerEvents(new EventListener(), $this);	  
+      $this->getServer()->getPluginManager()->registerEvents($this, $this);  
       $this->BetterPearl();
       $this->getScheduler()->scheduleRepeatingTask(new HAlwaysDayTask(), 40);
       @mkdir($this->getDataFolder());
