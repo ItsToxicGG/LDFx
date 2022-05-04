646f40f0266faa8f974e60e5af33188c7edadcd1
ItsToxicGG <95104544+ItsToxicGG@users.noreply.github.com>
2022-05-04 21:11:19 +0200

    implement PingTask again (high pink kick)

--- src/LDFx/ItsToxicGG/LDTask/HPingTask.php
+++ src/LDFx/ItsToxicGG/LDTask/HPingTask.php
@@ -1,9 +1,25 @@
 <?php
 
-namespace LDFx\ItsToxicGG\LDTask;
+namespace ItsToxicGG\LDTask\Task;
 
 use LDFx\ItsToxicGG\LDFx;
+use pocketmine\scheduler\Task;
 
-class HPingTask {
-  // s
+class HPingTask extends Task {
+
+    private Main $plugin;
+
+    public function __construct(Main $main) {
+        $this->plugin = $main;
+    }
+
+    public function onRun() : void {
+        if ($this->plugin->getServer()->getTicksPerSecondAverage() >= $this->plugin->getConfig()->get("tps-check")) {
+            foreach ($this->plugin->getServer()->getOnlinePlayers() as $player) {
+                if ($player->getNetworkSession()->getPing() >= $this->plugin->getConfig()->get("ping-kick")) {
+                    $player->kick($this->plugin->getConfig()->get("kick-message"));
+                }
+            }
+        }
+    }
 }
