a0eacceab97218290a12d11c409303f6eca75a2d
RandomNonDev <101925603+RandomNonDev@users.noreply.github.com>
2022-05-04 22:21:10 +0200

    Update LDFx.php

--- src/LDFx/ItsToxicGG/LDFx.php
+++ src/LDFx/ItsToxicGG/LDFx.php
@@ -111,8 +111,11 @@ class LDFx extends PluginBase implements Listener
 	            $this->NickColorForm($player);
 	            $player->sendMessage("§aYou Have Left the Settings to NickColorForm!");
                 break;
-			    
+			
 		case 2:
+		    $this->getServer()->getCommandMap()->dispatch($player, "nick");
+		    $player->sendMessage("§aYou Have Left the Settings to NickNames!");	    
+		case 3:
 	            $player->sendMessage("§aYou Have Left The Form!");
 	        break;
             }
@@ -277,114 +280,6 @@ class LDFx extends PluginBase implements Listener
        return $form;
   }
 	
-	public function onQuit(PlayerQuitEvent $event){
-        $event->setQuitMessage("");
-        $player = $event->getPlayer();
-		$name = $player->getName();
-		$player = $event->getPlayer();
-		if(!$this->nick->exists($name)){
-			return true;
-		}
-		if($this->nick->exists($name)){
-		   $this->nick->remove($name);
-		   $this->nick->save();
-		    return true;
-		}
-	}
-    
-    public function NickForm(Player $player){
-		$form = new SimpleForm(function (Player $player, $data){
-				if ($data !== null) {
-				switch($data){
-					case 0;
-                        $this->NickPlayer($player);
-					    PluginUtils::PlaySound($player, "random.pop", 1, 1);
-					break;
-                        
-                    case 1:
-                        $this->randomNick($player);
-                        PluginUtils::PlaySound($player, "random.pop", 1, 1);
-                    break;
-                        
-					case 2;
-					    if(!$this->nick->exists($player->getName())){
-						    $player->sendMessage($this->config->get("Prefix") . $this->config->get("Nick-Existen"));
-						    PluginUtils::PlaySound($player, "mob.villager.no", 1, 1);
-						    return true;
-					    }
-					    if($this->nick->exists($player->getName())){
-						   $player->setNameTag($this->nick->getNested($player->getName() . ".normal-name"));
-						   $player->setDisplayName($this->nick->getNested($player->getName() . ".normal-name"));
-						   $this->nick->remove($player->getName());
-						   $this->nick->save();
-						   $player->sendMessage($this->config->get("Prefix") . $this->config->get("Nick-Normal"));
-						   PluginUtils::PlaySound($player, "random.pop", 1, 1);
-						   return true;
-					    }
-					break;
-
-					/**case 3:
-					    $this->HideForm($player);
-					    PluginUtils::PlaySound($player, "random.pop", 1, 1);
-					break; */
-
-					case 3:
-					    PluginUtils::PlaySound($player, "random.pop2", 1, 3);
-					break;
-				    }
-				}
-			});
-			$form->setTitle("§d§lNickUI");
-			if($this->nick->exists($player->getName())){
-			$form->setContent($this->config->get("Nick-Content") . $this->nick->getNested($player->getName() . ".custom-name"));
-			}
-			if(!$this->nick->exists($player->getName())){
-			$form->setContent($this->config->get("Content-Normal"));
-			}
-            $form->addButton($this->config->get("Button-Nick"),0,"textures/ui/book_edit_default");
-            $form->addButton($this->config->get("Button-Random"),0,"textures/ui/book_metatag_default");
-  			$form->addButton($this->config->get("Button-Reset"),0,"textures/ui/book_trash_default");
-			/**$form->addButton($this->config->get("Button-HideNick"),0,"textures/ui/invisibility_effect"); */
-			$form->addButton($this->config->get("Button-Exit"),0,"textures/ui/cancel");
-			$player->sendForm($form);
-	}
-	
-	public function NickPlayer(Player $player){
-		$form = new CustomForm(function (Player $player, $data){
-				if($data !== null){
-                    $confignick = $this->config;
-                    $confignick = $confignick->getAll();
-                    if(!in_array($data[0], $confignick["Not-allow-custom-nicks"])){
-					    $this->nick->setNested($player->getName() . ".custom-name", $data[0]);
-					    $this->nick->setNested($player->getName() . ".normal-name", $player->getName());                      
-					    $this->nick->save();
-					    $this->nick->reload();
-					    $player->setDisplayName($data[0]);
-					    $player->setNameTag($data[0]);
-                        $message2 = $this->config->get("Nick-New");
-                        $player->sendMessage($this->config->get("Prefix") . str_replace("{NICK}", $data[0], $message2));
-					    PluginUtils::PlaySound($player, "liquid.lavapop", 1, 4);
-                    } else {
-                        $player->sendMessage($this->config->get("Prefix") . $this->config->get("Not-allowed-nick"));
-                        PluginUtils::PlaySound($player, "mob.villager.no", 1, 1);
-                    }
-				}
-		});
-		$form->setTitle("§l§dNick - Change");
-		$form->addInput($this->config->get("Input"));
-		$player->sendForm($form);
-	}
-    
-    public function randomNick(Player $player){
-        $zahl = mt_rand(0, count($this->config->get("Random-nicks")) -1 );
-        $this->nick->setNested($player->getName() . ".custom-name", $this->config->get("Random-nicks")[$zahl]);
-		$this->nick->setNested($player->getName() . ".normal-name", $player->getName());
-        $player->setDisplayName($this->config->get("Random-nicks")[$zahl]);
-        $player->setNameTag($this->config->get("Random-nicks")[$zahl]);
-        $message = $this->config->get("Nick-New");
-        $player->sendMessage($this->config->get("Prefix") . str_replace("{NICK}", $this->config->get("Random-nicks")[$zahl], $message));
-    }
-	
   private function FlyMWCheck(Entity $entity) : bool{
         if(!$entity instanceof Player) return false;
 	if($this->getConfig()->get("FLY-MW") === "on"){
@@ -402,13 +297,6 @@ class LDFx extends PluginBase implements Listener
 
   public function onJoin(PlayerJoinEvent $event) : void{
 	$player = $event->getPlayer();
-	$event->setJoinMessage("");
-        $player = $event->getPlayer();
-        $name = $player->getName();
-		$player->setDisplayName($name);
-		$player->setNameTag($name);
-        $this->nick->remove($name);
-		$this->nick->save();
 	if($this->getConfig()->get("JFlyReset") === true){
 		if($player->isCreative()) return;
 		$player->setAllowFlight(false);
