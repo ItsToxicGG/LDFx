cedeb7131fcaf7d3668da7fefae52cda70cc47d7
DNTxH <101135927+DNTxH@users.noreply.github.com>
2022-05-25 19:36:56 +0300

    Update #1

--- src/LDFx/ItsToxicGG/LDCommand/SpawnCommand.php
+++ src/LDFx/ItsToxicGG/LDCommand/SpawnCommand.php
@@ -5,13 +5,20 @@ declare(strict_types=1);
 
 namespace LDFx\ItsToxicGG\LDCommands;
 
+use pocketmine\Server;
+use pocketmine\player\Player;
+use pocketmine\plugin\Plugin;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\player\Player;
-use pocketmine\Server;
 
-class SpawnCommand extends Command {
+use pocketmine\plugin\PluginOwned;
+
+use LDFx\ItsToxicGG\LDFx;
+
+class SpawnCommand extends Command implements PluginOwned{
+    
+    private $plugin;
 
     public function __construct() {
         parent::__construct("spawn", 'Teleport you to the server spawn!', null, ["hub", "lobby"]);
@@ -21,7 +28,18 @@ class SpawnCommand extends Command {
     public function execute(CommandSender $sender, string $commandLabel, array $args): void {
         if ($sender instanceof Player) {
             $sender->teleport(Server::getInstance()->getWorldManager()->getDefaultWorld()->getSafeSpawn());
+            } else {
+                $sender->sendMessage("Use this command in-game");
+            }
         }
+        return true;
+    }
+    
+    public function getPlugin(): Plugin{
+        return $this->plugin;
     }
 
+    public function getOwningPlugin(): LDFx{
+        return $this->plugin;
+    }
 }
