10973f98f878fedc5b7ea22c402b5860ddadb8f5
ItsToxicGG <95104544+ItsToxicGG@users.noreply.github.com>
2022-05-25 21:04:35 +0200

    Credits to DNTxH

---
+++ src/LDFx/ItsToxicGG/LDCommand/HubCommand.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LDFx\ItsToxicGG\LDCommand;
+
+use pocketmine\Server;
+use pocketmine\player\Player;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+use pocketmine\plugin\PluginOwned;
+
+use LDFx\ItsToxicGG\LDFx;
+
+class HubCommand extends Command implements PluginOwned {
+    
+    private $plugin;
+    
+    public function __construct(LDFx $plugin){
+        $this->plugin = $plugin; 
+        
+        parent::__construct("spawn", 'Teleport you to the server spawn!', null, ["hub", "lobby"]);
+        $this->setAliases(["hub", "lobby"]);
+    }
+    
+    public function execute(CommandSender $sender, string $commandLabel, array $args): void {
+        if ($sender instanceof Player) {
+            $sender->teleport(Server::getInstance()->getWorldManager()->getDefaultWorld()->getSafeSpawn());
+            } else {
+                $sender->sendMessage("Use this command in-game");
+            }
+        }
+
+    public function getOwningPlugin(): LDFx{
+        return $this->plugin;
+    }
+}
