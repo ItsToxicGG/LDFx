c6a07b54d8962b3fce09328e6928d36fc24a4676
ItsToxic <95104544+ItsToxicGG@users.noreply.github.com>
2022-06-13 20:39:13 +0200

    Implemention of command

---
+++ src/LDFx/ItsToxicGG/LDCommand/FriendCommand.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Friend\SandhyR;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+use LDFx\ItsToxicGG\LDForm\FriendsForm;
+
+class FriendCommand extends Command{
+
+    public function __construct()
+    {
+        parent::__construct("friend", "§r§fOpen Up Friends Form, Plugin By ItsToxicGG", "§cUse: /friends", ["friends"]);
+        $this->setAliases(["friend", "f"]);
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args)
+    {
+       if($sender instanceof Player){
+           new FriendsForm($sender);
+       } else {
+           $sender->sendMessage("You not a player");
+       }
+    }
+}
